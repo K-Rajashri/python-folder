2894e79dcc4ec56cd3129c0972d58c658de52f7b
Rajashri <krajashri1709@gmail.com>
2025-06-06 10:26:47 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,26 @@
+# Step 1
+class Employee:
+    def determine_weekly_salary(self, weekly_hours, wage):
+        salary = 40 * wage
+        print("\nThis EMPLOYEE worked {} hrs. Paid for 40 hrs (no overtime) at ${}/hr = ${}".format(
+            weekly_hours, wage, salary))
+        print("-------------------------------------------------\n")
+
+# Step 2
+class Contractor(Employee):
+    def determine_weekly_salary(self, weekly_hours, wage):
+        salary = weekly_hours * wage
+        print("\nThis CONTRACTOR worked {} hrs (w/ overtime) at ${}/hr = ${}".format(
+            weekly_hours, wage, salary))
+
+# Step 3
+def main():
+    hours, wage = 50, 70
+    person = Employee()
+    person.determine_weekly_salary(hours, wage)
+
+if __name__ == "__main__":
+    main()
+
+
+
